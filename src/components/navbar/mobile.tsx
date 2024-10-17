ac4c45b1c44e6090fb458917c54365ff136872db
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-10-17 22:03:55 +0100

    🐛 FIX: changing to icon

--- src/components/navbar/mobile.tsx
+++ src/components/navbar/mobile.tsx
@@ -2,15 +2,22 @@ import { Link } from "@/i18n/routing";
 import { LanguageSwitcher } from "../language-switcher";
 import { ThemeSwitcher } from "../theme-switcher";
 import { MobileNavigation } from "./mobile-navigation";
+import Image from "next/image";
 
 export function MobileNavbar() {
   return (
     <div className="flex w-full justify-between items-center sm:hidden">
       <div className="flex gap-4 items-center">
         <Link href="/" className="relative group">
-          <span className="font-jet-brains-mono text-4xl font-thin text-primary">
+          {/* <span className="font-jet-brains-mono text-4xl font-thin text-primary">
             L
-          </span>
+          </span> */}
+          <Image
+            alt="colossus"
+            src="/images/colossus-colored.svg"
+            width={20}
+            height={80}
+          />
         </Link>
         <MobileNavigation />
       </div>
